784916bc666f6fd55a5457d194df5d4f28798b8a
Mykola Babchenko <mykolababchenko@truckstop.com>
2020-10-17 01:53:21 +0300

    Changes namespace

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import {sass} from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'skeleton',
+  namespace: 'skeleton-webcomponent',
   outputTargets: [
     {
       type: 'dist',
